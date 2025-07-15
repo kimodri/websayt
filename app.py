81352c0b33f9d33e4af80d8c571596129ea4dace
unknown <studies.kimaudrey@gmail.com>
2025-07-15 09:37:50 +0800

    Added bunch of things

--- app.py
+++ app.py
@@ -8,7 +8,56 @@ def about():
 
 @app.route('/papers')
 def papers():
-    return render_template("papers.html")
+    titles = [
+        # "Predicting Quantum State Transitions Using Transformer-Based Architectures: A New Approach to Physical Law Discovery (fake)",
+        # "On the Bifurcation of Prime Topologies in Modular Infinity Groups (fake)",
+        # "Recursive Neural Compilers for Self-Improving Distributed Agents in Unstable Networks (fake)",
+        # "The Ontological Implications of Digital Consciousness in a Post-Existential Framework (fake)"
+        "Physics Paper (placeholder)", "Mathematics Papaer (placeholder)", "Computer Science Paper (placeholder)", "Philosophy Paper (placeholder)"
+    ]
+    authors = [
+        ["Magan, Kim Audrey B."],
+        ["Magan, Kim Audrey B."],
+        ["Magan, Kim Audrey B.", "Author 2"],
+        ["Magan, Kim Audrey B.", "Kokey"]
+    ]
+    dates = ["2030", "2028", "2050", "2026"]
+    tags = [
+        ["Physics", "QuantumMechanics", "MachineLearning", "Transformers", "PhysicalLawDiscovery", "QuantumAI"],
+        ["Mathematics", "AbstractAlgebra", "Topology", "ModularGroups", "NumberTheory", "MathematicalFiction"],
+        ["ComputerScience", "ArtificialIntelligence", "NeuralNetworks", "Compilers", "DistributedSystems", "AutonomousAgents"],
+        ["Philosophy", "Ontology", "DigitalConsciousness", "Existentialism", "PostHumanism", "PhilosophyOfMind"]
+    ]
+
+    papers = zip(titles, authors, dates, tags)
+
+    return render_template("papers.html", papers=papers)
+
+@app.route('/projects')
+def projects():
+
+    img = ['/static/img/donenate.jpg', '/static/img/analysis.png']
+    project_names = ['DONEate', 'Data Analyses']
+
+    descriptions = ['A Donation Tracker System that creates, manages, and monitors donation campaigns.', 'Collections of data projects for analyses which\
+                    includes data cleaning and visualization.']
+    
+    tags = [
+        ['Flask', 'HTML', 'CSS', 'JavaScript'],
+        ['Python', 'DataCleaning', 'DataViz', 'Analysis']
+        ]
+    
+    links = ['https://github.com/kimodri/donation-tracker', 'https://github.com/kimodri/data-projects']
+
+
+    projects = zip(img, project_names, descriptions, tags, links)
+
+    return render_template('projects.html', projects = projects)
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
